a451917cb3ca65876b01262d1dd6a7794a972b9b
Benjy Malca <bnmalcabis@gmail.com>
2017-09-04 12:14:27 -0500

    Changed format of urlpatterns

--- generics/urls.py
+++ generics/urls.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url #include,
+from django.conf.urls import url #include,
+
+from generics import views
 
 # import logging
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.INFO)
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^task_api$', 'generics.views.task_api', name="task_api"),
-    url(r'^celery_test$', 'generics.views.celery_test', name="celery_test"),
-    url(r'^messages_api$', 'generics.views.messages_api', name="messages_api"),
-    )
-
+urlpatterns = [
+    url(r'^task_api$', views.task_api, name="task_api"),
+    url(r'^celery_test$', views.celery_test, name="celery_test"),
+    url(r'^messages_api$', views.messages_api, name="messages_api"),
+]
